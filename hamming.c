906147016e347d9d514393572a85a75c626dd840
Aldrin Martoq <aldrin@martoq.cl>
2012-06-24 22:37:25 -0400

    Corregida, copiando la idea de multiplicar por los números ya generados de Mauricio Quezada

--- hamming.c
+++ hamming.c
@@ -1,34 +1,34 @@
 /*********************************************************************
-hamming.c
-
-Compilación performante:
-$ clang -O3 hamming.c -o hamming
-
-Compilación para depuración:
-$ clang -DDEBUG hamming.c -o hamming
-
-Ejecución:
-$ time ./hamming 7 13 19 1000000
-3993073
-
-real	0m0.010s
-user	0m0.007s
-sys	0m0.002s
-
-DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-Version 2, December 2004 
-
-Copyright (C) 2012 Aldrin Martoq
-
-Everyone is permitted to copy and distribute verbatim or modified 
-copies of this license document, and changing it is allowed as long 
-as the name is changed. 
-
-DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
-0. You just DO WHAT THE FUCK YOU WANT TO.
-**********************************************************************/
+ hamming.c - versión corregida pero ocupa mucha memoria
+ 
+ Compilación performante:
+ $ clang -O3 hamming.c -o hamming
+ 
+ Compilación para depuración:
+ $ clang -DDEBUG hamming.c -o hamming
+ 
+ Ejecución:
+ $ time ./hamming 7 13 19 1000000
+ 4474371506000938917
+ 
+ real 0m0.137s
+ user 0m0.007s
+ sys  0m0.007s
+ 
+ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ Version 2, December 2004 
+ 
+ Copyright (C) 2012 Aldrin Martoq
+ 
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+ 
+ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+ 
+ 0. You just DO WHAT THE FUCK YOU WANT TO.
+ **********************************************************************/
 
 
 
@@ -51,17 +51,22 @@ NUM min(NUM a, NUM b, NUM c) {
 
 NUM h(NUM p1, NUM p2, NUM p3, NUM n) {
     NUM c1 = p1, c2 = p2, c3 = p3;
-
-    printf_debug("%5s %10s | %10s %10s %10s\n", "n", "h", "p1", "p2", "p3");
-
+    int i1 = 1, i2 = 1, i3 = 1;
+    NUM *arr = calloc(sizeof(NUM), n);
+    
+    
+    printf_debug("%5s %10s | %10s %5s %10s %5s %10s %5s\n", "n", "h", "p1", "i1", "p2", "i2", "p3", "i3");
+    
     NUM z = 0;
     for (NUM i = 1; i <= n; i++) {
-        z = min(c1, c2, c3);
-        printf_debug("%5llu %10lli | %10llu %10llu %10llu\n", i, z, c1, c2, c3);
-        if (c1 <= z) c1 += p1;
-        if (c2 <= z) c2 += p2;
-        if (c3 <= z) c3 += p3;
+        arr[i] = z = min(c1, c2, c3);
+        printf_debug("%5llu %10lli | %10llu %5d %10llu %5d %10llu %5d\n", i, z, c1, i1, c2, i2, c3, i3);
+        if (c1 <= z) c1 = p1*arr[i1++];
+        if (c2 <= z) c2 = p2*arr[i2++];
+        if (c3 <= z) c3 = p3*arr[i3++];
     }
+    
+    free(arr);
     return z;
 }
 
